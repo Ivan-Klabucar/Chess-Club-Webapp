42eaefea35c0bd4878850e612a05bf337fea20bd
Ivar <ivar.cmrecak@gmail.com>
2020-10-22 10:00:41 +0200

    Setup za autentikaciju

--- IzvorniKod/SahovskiKlub/urls.py
+++ IzvorniKod/SahovskiKlub/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from django.contrib.admin import views
-from .views import HomeView
+from django.contrib.auth import views
+from global_login_required import login_not_required
+from .views import HomeView, sign_up
 
 urlpatterns = [
-    path('', HomeView.as_view())
+    path('', HomeView.as_view()),
+    path('login', login_not_required(views.LoginView.as_view())),
+    path('register', sign_up, name="sign-up"),
 ]
--- IzvorniKod/SahovskiKlub/views.py
+++ IzvorniKod/SahovskiKlub/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.views import View
 
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
 # Create your views here
 
 class HomeView(View):
     def get(self, request):
         return render(request, 'index.html')
+
+def sign_up(request):
+    context = {}
+    form = UserCreationForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return render(request, 'index.html')
+    context['form'] = form
+    return render(request, 'registration/sign_up.html', context)
