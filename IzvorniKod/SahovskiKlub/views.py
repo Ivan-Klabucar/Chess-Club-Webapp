073e5f23e4d7a57243275ddc53850d7254ef10be
Ivar <ivar.cmrecak@gmail.com>
2020-10-22 10:27:29 +0200

    Uljepsan frontend

--- IzvorniKod/SahovskiKlub/views.py
+++ IzvorniKod/SahovskiKlub/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth import login
 
 class HomeView(View):
     def get(self, request):
-        return render(request, 'index.html')
+        return render(request, 'homepage.html')
 
 def sign_up(request):
     context = {}
@@ -18,6 +18,6 @@ def sign_up(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return render(request, 'index.html')
+            return render(request, 'homepage.html')
     context['form'] = form
     return render(request, 'registration/sign_up.html', context)
