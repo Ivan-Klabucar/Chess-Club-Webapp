8f3e4fe4ccf885c7ce22c36a3a7aa4a94a78623b
Ivar <ivar.cmrecak@gmail.com>
2020-10-22 15:12:50 +0200

    Znatno poboljsana funkcionalnost

--- IzvorniKod/SahovskiKlub/urls.py
+++ IzvorniKod/SahovskiKlub/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from django.contrib.auth import views
-from global_login_required import login_not_required
 from .views import HomeView, sign_up
 
 urlpatterns = [
     path('', HomeView.as_view()),
-    path('login', login_not_required(views.LoginView.as_view())),
+    path('logout', views.LogoutView.as_view()),
+    path('login', views.LoginView.as_view()),
     path('register', sign_up, name="sign-up"),
 ]
