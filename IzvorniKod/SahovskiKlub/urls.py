525bd1b85dd1119404f8c0a040a27912cd2cbfa4
Ivar <ivar.cmrecak@gmail.com>
2020-11-06 19:28:09 +0100

    view registracije prebacen u oblik klase

--- IzvorniKod/SahovskiKlub/urls.py
+++ IzvorniKod/SahovskiKlub/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from django.contrib.auth import views
-from .views import HomeView, sign_up
+from .views import HomeView, RegisterView
 
 urlpatterns = [
     path('', HomeView.as_view()),
     path('logout', views.LogoutView.as_view()),
     path('login', views.LoginView.as_view()),
-    path('register', sign_up, name="sign-up"),
+    path('register', RegisterView.as_view()),
 ]
--- IzvorniKod/SahovskiKlub/views.py
+++ IzvorniKod/SahovskiKlub/views.py
@@ -1,23 +1,28 @@
 from django.shortcuts import render
 from django.views import View
-
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 
-# Create your views here
 
 class HomeView(View):
     def get(self, request):
         return render(request, 'homepage.html')
 
-def sign_up(request):
-    context = {}
-    form = UserCreationForm(request.POST or None)
-    if request.method == "POST":
+
+class RegisterView(View):
+    def get(self, request):
+        context = {}
+        form = UserCreationForm(request.POST or None)
+        context['form'] = form
+        return render(request, 'registration/sign_up.html', context)
+
+    def post(self, request):
+        context = {}
+        form = UserCreationForm(request.POST or None)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return render(request, 'homepage.html')
-    context['form'] = form
-    return render(request, 'registration/sign_up.html', context)
+        print(form.errors)
+        context['form'] = form
+        return render(request, 'registration/sign_up.html', context)
