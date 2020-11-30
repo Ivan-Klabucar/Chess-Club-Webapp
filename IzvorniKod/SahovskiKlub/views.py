1e6c6cfdddf91ce287416b87d2e26803813de62f
mihamih <ms51741@fer.hr>
2020-11-30 20:11:51 +0100

    dodana stranica za pregled treninga

--- IzvorniKod/SahovskiKlub/views.py
+++ IzvorniKod/SahovskiKlub/views.py
@@ -122,3 +122,33 @@ class TacticRevisionView(View):
         print("Revision #{} was rejected".format(request.POST.get('id', '')))
       return HttpResponse('sve pet')
 
+
+
+class TreninziView(View):
+    def get(self, request):
+        context = {
+            "listaTreninga" : [
+                {
+                                "organizator" : "Trener1",
+                                "vrijemePocetka" : "16:00",
+                                "vrijemeZavrsetka": "18:00",
+								"datum": "10.12.2020.",
+                                "opis" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ex metus, dignissim eget mi nec, ullamcorper auctor diam. Donec tincidunt massa quis risus vulputate sollicitudin. Curabitur iaculis mattis tempor. Suspendisse finibus ante sit amet finibus sodales. Proin porttitor fringilla tellus vitae dignissim. Morbi eget massa metus. Proin mollis tellus quis dignissim pulvinar. Nam suscipit nisi mattis nisi euismod consectetur. Sed nec nisl placerat, imperdiet nunc a, ultricies mi. Morbi eget fermentum neque. Quisque et erat ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas."
+                },
+                {
+                                "organizator" : "Trener2",
+                                "vrijemePocetka" : "16:00",
+                                "vrijemeZavrsetka": "18:00",
+								"datum": "11.12.2020.",
+                                "opis" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ex metus, dignissim eget mi nec, ullamcorper auctor diam. Donec tincidunt massa quis risus vulputate sollicitudin. Curabitur iaculis mattis tempor. Suspendisse finibus ante sit amet finibus sodales. Proin porttitor fringilla tellus vitae dignissim. Morbi eget massa metus. Proin mollis tellus quis dignissim pulvinar. Nam suscipit nisi mattis nisi euismod consectetur. Sed nec nisl placerat, imperdiet nunc a, ultricies mi. Morbi eget fermentum neque. Quisque et erat ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas."
+                },
+                {
+                                "organizator" : "Trener3",
+                                "vrijemePocetka" : "16:00",
+                                "vrijemeZavrsetka": "18:00",
+								"datum": "12.12.2020.",
+                                "opis" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ex metus, dignissim eget mi nec, ullamcorper auctor diam. Donec tincidunt massa quis risus vulputate sollicitudin. Curabitur iaculis mattis tempor. Suspendisse finibus ante sit amet finibus sodales. Proin porttitor fringilla tellus vitae dignissim. Morbi eget massa metus. Proin mollis tellus quis dignissim pulvinar. Nam suscipit nisi mattis nisi euismod consectetur. Sed nec nisl placerat, imperdiet nunc a, ultricies mi. Morbi eget fermentum neque. Quisque et erat ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas."
+                }
+                        ],
+                }
+        return render(request, 'treninzi.html', context)
