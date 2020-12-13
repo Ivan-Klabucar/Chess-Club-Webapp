47d679c6905ad93de2775b6783f7b3efd5aeed54
mihamih <ms51741@fer.hr>
2020-12-13 22:40:23 +0100

    listanje treninga povezano s bazom

--- IzvorniKod/SahovskiKlub/views/trening_views.py
+++ IzvorniKod/SahovskiKlub/views/trening_views.py
@@ -6,39 +6,29 @@ from django.http import HttpResponse
 
 class TreninziView(View):
     def get(self, request):
+        treninzi = Trening.objects.all()
+        listaTreninga = []
+        
+        for trening in treninzi:
+            treningObj = {
+                            "org": trening.organizator,
+                            "vrijemeP": trening.vrijemePocetka,
+                            "vrijemeZ": trening.vrijemeZavrsetka,
+                            "opis": trening.opisTreninga
+                            }
+            treninziObj.append(treningObj)
+        
+        
         context = {
-            "listaTreninga": [
-                {
-                    "idTreninga": "12",
-                    "organizator": "Trener1",
-                    "vrijemePocetka": "16:00",
-                    "vrijemeZavrsetka": "18:00",
-                    "datum": "10.12.2020.",
-                    "opis": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ex metus, dignissim eget mi nec, ullamcorper auctor diam. Donec tincidunt massa quis risus vulputate sollicitudin. Curabitur iaculis mattis tempor. Suspendisse finibus ante sit amet finibus sodales. Proin porttitor fringilla tellus vitae dignissim. Morbi eget massa metus. Proin mollis tellus quis dignissim pulvinar. Nam suscipit nisi mattis nisi euismod consectetur. Sed nec nisl placerat, imperdiet nunc a, ultricies mi. Morbi eget fermentum neque. Quisque et erat ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas."
-                },
-                {
-                    "idTreninga": "34",
-                    "organizator": "Trener2",
-                    "vrijemePocetka": "16:00",
-                    "vrijemeZavrsetka": "18:00",
-                    "datum": "11.12.2020.",
-                    "opis": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ex metus, dignissim eget mi nec, ullamcorper auctor diam. Donec tincidunt massa quis risus vulputate sollicitudin. Curabitur iaculis mattis tempor. Suspendisse finibus ante sit amet finibus sodales. Proin porttitor fringilla tellus vitae dignissim. Morbi eget massa metus. Proin mollis tellus quis dignissim pulvinar. Nam suscipit nisi mattis nisi euismod consectetur. Sed nec nisl placerat, imperdiet nunc a, ultricies mi. Morbi eget fermentum neque. Quisque et erat ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas."
-                },
-                {
-                    "idTreninga": "56",
-                    "organizator": "Trener3",
-                    "vrijemePocetka": "16:00",
-                    "vrijemeZavrsetka": "18:00",
-                    "datum": "12.12.2020.",
-                    "opis": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ex metus, dignissim eget mi nec, ullamcorper auctor diam. Donec tincidunt massa quis risus vulputate sollicitudin. Curabitur iaculis mattis tempor. Suspendisse finibus ante sit amet finibus sodales. Proin porttitor fringilla tellus vitae dignissim. Morbi eget massa metus. Proin mollis tellus quis dignissim pulvinar. Nam suscipit nisi mattis nisi euismod consectetur. Sed nec nisl placerat, imperdiet nunc a, ultricies mi. Morbi eget fermentum neque. Quisque et erat ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas."
-                }
-            ],
+            "listaTreninga": treninziObj
         }
         return render(request, 'treninzi.html', context)
 
     def post(self, request):
-        print(request.POST.get('idUsera'))
-        print(request.POST.get('idTreninga'))
+        idUser = request.POST.get('idUsera')
+        idTrening = request.POST.get('idTreninga')
+        prijava = PrijavaTrening(user=idUser, trening=idTrening)
+        prijava.save()
         return redirect('/treninzi')
 
 
