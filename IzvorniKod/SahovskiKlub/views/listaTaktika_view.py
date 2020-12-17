4e3cf19c48b775b7abaaeb48ce275020dff74d0f
Petar Gabrijel Kedmenec <pk51606@fer.hr>
2020-12-17 16:53:19 +0100

    view listaTaktika spojen na bazu, template listaDnevnihTaktika poboljsan

--- IzvorniKod/SahovskiKlub/views/listaTaktika_view.py
+++ IzvorniKod/SahovskiKlub/views/listaTaktika_view.py
@@ -1,25 +1,33 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from django.http import HttpResponse
+from ..models import *
 
 class ListaTaktikaView(View):
     def get(self, request):
+        taktike = Taktika.objects.order_by('id')
+        lista_taktika = []
+
+        for taktika in taktike:
+            if RjesenjeTaktike.objects.filter(taktika=taktika, user=request.user).exists():
+                taktika_obj = {
+                    "autor": taktika.user.username,
+                    "datum": taktika.createdAt,
+                    "idTaktika": taktika.id,
+                    "ime": taktika.ime,
+                    "vrijeme": RjesenjeTaktike.objects.filter(taktika=taktika, user=request.user).first().vrijeme,
+                    "rjeseno": "1"
+                }
+            else:
+                taktika_obj = {
+                    "autor": taktika.user.username,
+                    "datum": taktika.createdAt,
+                    "idTaktika": taktika.id,
+                    "ime": taktika.ime
+                }
+            lista_taktika.append(taktika_obj)
+
         context = {
-            "listaTaktika": [
-                {
-                    "autor": "Marko",
-                    "datum": "17.8.2019"
-                },
-                {
-                    "autor": "Ivo",
-                    "datum": "19.10.2020"
-                }
-            ],
-            "listaTaktikeIstaknute": [
-                {
-                    "autor": "Bruno",
-                    "datum": "19.10.2021"
-                }
-            ]
+            "listaTaktika": lista_taktika
         }
         return render(request, 'listaDnevnihTaktika.html', context)
