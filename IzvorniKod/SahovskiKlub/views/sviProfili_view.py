1e0c8d930ee0c3b3634856cc2e28bac416ece825
franleontic <fran.leontic@yahoo.com>
2021-01-08 11:55:32 +0100

    Popravljeno dohvacanje profila

--- IzvorniKod/SahovskiKlub/views/sviProfili_view.py
+++ IzvorniKod/SahovskiKlub/views/sviProfili_view.py
@@ -11,7 +11,7 @@ class SviProfiliView(View):
     def get(self, request):
         if not request.user.is_authenticated:
             return render_error(request, 'Niste prijavljeni', 400)
-        currentuser = Profil.objects.get(id=request.user.id)
+        currentuser = Profil.objects.get(user_id=request.user.id)
         if currentuser.zabranjenPristup:
             return render_error(request, 'Zabranjen pristup', 400)
         if not currentuser.admin:
@@ -28,19 +28,19 @@ class DetaljanProfilView(View):
     def get(self, request):
         if not request.user.is_authenticated:
             return render_error(request, 'Niste prijavljeni', 400)
-        currentuser = Profil.objects.get(id=request.user.id)
+        currentuser = Profil.objects.get(user_id=request.user.id)
         if currentuser.zabranjenPristup:
             return render_error(request, 'Zabranjen pristup', 400)
         if not currentuser.admin:
             return render_error(request, 'Morate biti admin kako biste pristupili prikazu profila', 400)
 
-        user_id = request.GET.get('id', '')
-        if not user_id:
+        detaljan_id = request.GET.get('id', '')
+        if not detaljan_id:
             return render_error(request, 'Niste odabrali profil za detaljan prikaz', 400)
-        if not Profil.objects.filter(id=user_id).exists():
+        if not Profil.objects.filter(user_id=detaljan_id).exists():
             return render_error(request, 'Profil s zadanim id-om ne postoji', 400)
-        user = Profil.objects.get(id=user_id)
-        activityList = list(Aktivnost.objects.filter(user_id=user_id).order_by('vrijemeAktivnosti'))
+        user = Profil.objects.get(user_id=detaljan_id)
+        activityList = list(Aktivnost.objects.filter(user_id=detaljan_id).order_by('vrijemeAktivnosti'))
         context = {
             "detaljan_user": user,
             "listaAktivnosti": activityList
@@ -49,12 +49,12 @@ class DetaljanProfilView(View):
 
     def post(self, request):
         if request.POST.get('enable'):
-            user_id = request.POST.get('enable')
-            Profil.objects.filter(id=user_id).update(zabranjenPristup=False)
+            detaljan_id = request.POST.get('enable')
+            Profil.objects.filter(user_id=detaljan_id).update(zabranjenPristup=False)
         elif request.POST.get('disable'):
-            user_id = request.POST.get('disable')
-            Profil.objects.filter(id=user_id).update(zabranjenPristup=True)
-        return redirect('/detaljanProfil?id=' + user_id)
+            detaljan_id = request.POST.get('disable')
+            Profil.objects.filter(user_id=detaljan_id).update(zabranjenPristup=True)
+        return redirect('/detaljanProfil?id=' + detaljan_id)
 
 
 
