2999e871e0e08c846fde24a136e11cdc7a1025eb
Ivan Klabučar <ivan.klabucar@fer.hr>
2021-01-12 14:42:43 +0100

    Klabucar dojava pogreske ide pravom treneru na review test

--- IzvorniKod/SahovskiKlub/tests.py
+++ IzvorniKod/SahovskiKlub/tests.py
@@ -2,8 +2,9 @@ from django.test import TestCase, RequestFactory
 from django.contrib.auth.models import AnonymousUser, User
 from django.utils import timezone
 from .views.main_views import HomeView
+from .views.taktika_views import TacticRevisionView
 from .views.prikazNovosti_view import NovostiView
-from .models import Trening, DojavaPogreske, Turnir, Novost
+from .models import Trening, DojavaPogreske, Turnir, Novost, Profil, Taktika
 import datetime
 
 class RequestHomepageTest(TestCase):
@@ -94,3 +95,24 @@ class UnitTurnirTest(TestCase):
     def test_details(self):
         turnir = Turnir(organizator=self.user, brojSudionika=self.brojSudionika)
         turnir.save()
+
+
+class RequestTacticErrorReportToRightUserTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.normal_user = User.objects.create_user(
+            username='John', email='john@hotmail.com', password='top_secret')
+        self.trener = User.objects.create_user(
+            username='trener', email='trener@hotmail.com', password='top_secret')
+        self.trener.profil.trener = True
+
+    def test_details(self):
+        vrijeme = datetime.datetime.now(tz=timezone.utc)
+        taktika = Taktika(ime="laka_taktika", createdAt=vrijeme, user=self.trener, initConfig="k7/8/8/8/5n2/8/8/1K3Q2 w KQkq - 0 1", movesWhite="Qh1+,Qe4,Qxf4", movesBlack="Ka7,Ka6", tezina=1, brojGlasova=1, validnost=True, vidljivost=True)
+        taktika.save()
+        dojava_pogreske = DojavaPogreske(taktika=taktika, userDojave=self.normal_user, userRevizija=self.trener, predlozeniTijek="Qf3+,Qxf4<W|B>Ka7", opis="vrlo informativan opis")
+        dojava_pogreske.save()
+        request = self.factory.get(f'/revidiranjeTaktike?id={dojava_pogreske.id}')
+        request.user = self.trener
+        response = TacticRevisionView.as_view()(request)
+        self.assertEqual(response.status_code, 200)
